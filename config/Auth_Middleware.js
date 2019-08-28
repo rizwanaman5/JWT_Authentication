8de2948c3a193838e5b9de30e6d19488454b6b7c
Rizwan Aman <rizwanaman5@gmail.com>
2019-08-28 15:04:17 +0530

    moved auth middleware to config folder

--- config/Auth_Middleware.js
+++ config/Auth_Middleware.js
@@ -1,15 +1,39 @@
-// var express = require('express');
-// var router = express.Router();
-// const db = require('../models');
-// const User = require('../models/user')
-// const jwt = require('jsonwebtoken');
-// const bcrypt = require('bcryptjs');
+var express = require('express');
+const jwt = require('jsonwebtoken');
 
-// // Verify Token function
+// Verify Token function
 
-// // Format of the Token
-// // Authorization: Bearer {token}
+// Format of the Token
+// Authorization: Bearer {token}
+
+function verifyToken(req, res, next) {
+    const bearerHeader = req.headers['authorization'];
+    // Checking token
+    // check to see that is not undefined 
+    console.log("i reached verify token");
+    if (typeof bearerHeader !== 'undefined') {
+        // Split at the space
+        const bearer = bearerHeader.split(' ');
+        // get token from the second index of the array
+        const bearerToken = bearer[1];
+        // Set the token
+        req.token = bearerToken;
+
+        jwt.verify(req.token, 'Motu', (err, authData) => {
+            if (err) {
+                res.json({ error: err, msg: "Invalid token" });
+            } else {
+                console.log("i reached else");
+
+                next()
+            }
+        })
+
+    } else {
+        res.json({ error: 'There is no token' })
+    }
+}
 
 
 
-// module.exports = router;
+module.exports = verifyToken;
--- routes/users.js
+++ routes/users.js
@@ -3,46 +3,13 @@ var router = express.Router();
 const db = require('../models');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
-const passport = require('passport')
-// const verifyToken = require('../config/Auth_Middleware')
-// passport.use(JwtStrategy);
+const verifyToken = require('../config/Auth_Middleware')
 
-/* GET users listing. */
 router.get('/', verifyToken, (req, res) => {
-  jwt.verify(req.token, 'Motu', (err, authData) => {
-    if (err) {
-      res.send(err)
-    } else {
-      res.json({
-        message: 'respond with a resource',
-        authData
-      });
-    }
-  })
+  console.log("i reached get");
+  res.json({
+    message: 'respond with a resource',
+  });
 });
-
-
-
-// Verify Token function
-
-// Format of the Token
-// Authorization: Bearer {token}
-
-function verifyToken(req, res, next) {
-  const bearerHeader = req.headers['authorization'];
-  // Checking token
-  // check to see that is not undefined 
-  if (typeof bearerHeader !== 'undefined') {
-    // Split at the space
-    const bearer = bearerHeader.split(' ');
-    // get token from the second index of the array
-    const bearerToken = bearer[1];
-    // Set the token
-    req.token = bearerToken;
-    next();
-  } else {
-    res.json({ error: 'Invalid Token' })
-  }
-}
 
 module.exports = router;
